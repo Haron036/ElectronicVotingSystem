911db7f2bd556fbc53ce346809816d98f21f9c9f
Haron036 <haronkipkirui2@gmail.com>
2025-09-22 12:38:42 +0300

    changes

--- Backend/VotingSystem/src/main/java/com/example/VotingSystem/Security/CorsConfig.java
+++ Backend/VotingSystem/src/main/java/com/example/VotingSystem/Security/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // frontend
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","https://electronicvotingkenya-frontend.onrender.com")); // frontend
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
