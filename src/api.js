f6ac3c4f67d0bbd0e0e3944a163f1e55299a03e7
Haron036 <haronkipkirui2@gmail.com>
2025-09-23 11:50:22 +0300

    changes

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 // api.js
 import axios from "axios";
-
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = `${import.meta.env.VITE_API_URL}/api`;
 
 const api = axios.create({
   baseURL: API_URL,
