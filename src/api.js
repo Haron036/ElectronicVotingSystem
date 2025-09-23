fe62c368a8fe6ee2eb1db5912a44080b948243d0
Haron036 <haronkipkirui2@gmail.com>
2025-09-23 16:59:11 +0300

    changes

--- src/api.js
+++ src/api.js
@@ -9,7 +9,7 @@ const api = axios.create({
 // Add token to requests if stored
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("authToken");
+    const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
